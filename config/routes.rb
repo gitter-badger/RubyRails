ce3082a6b7b33f9990bca07efeebb52a13d437dc
Anika Jaffara <anika.jaffara@gmail.com>
2015-07-27 16:45:06 -0500

    Updated pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :pins
   devise_for :users
   root "pages#home"
 	get "about" => "pages#about"
 	get "pinteresting" => "pages#pinteresting"
 	get "bootstrap" => "pages#bootstrap"
 	get "bourbon" => "pages#bourbon"
+	get "foundation" => "pages#foundation"
 	
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
